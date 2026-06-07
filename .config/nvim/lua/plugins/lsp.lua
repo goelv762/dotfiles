3d55f2b6cd5fe981b70a07bd8308079a97b99ebb
Varun Goel <goel.varunn@gmail.com>
2026-06-07 17:54:05 +1000

    clangd lsp stuff

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,6 +2,14 @@ vim.pack.add({
 	'https://github.com/neovim/nvim-lspconfig',
 })
 
+-- prevents includes
+vim.lsp.config('clangd', {
+    cmd = {
+        'clangd',
+        '--header-insertion=never',
+    },
+})
+
 vim.lsp.enable({
 	'lua_ls',
 	'clangd'
